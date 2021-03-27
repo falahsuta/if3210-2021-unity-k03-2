93436f5b85d6526e9d1947b0bcc023468430cd43
Arief Darmawan <ariefdarmawantantriady@gmail.com>
2021-03-27 23:04:58 +0700

    ObjectPooler and IPooledObject script and shop button + script

---
+++ Assets/ShopScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopScript : MonoBehaviour
+{
+    public GameObject shopPanel;
+    bool show = false;
+
+    public void onClick()
+    {
+        shopPanel.SetActive(!show);
+        show = !show;
+    }
+}
